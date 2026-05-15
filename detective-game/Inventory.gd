20eed70cc0f96a9820b7b52e182db44a0b0faaa0
Martin Braun <martinbraun1997@gmail.com>
2026-05-15 13:36:45 +0200

    added inventory and restructured

--- detective-game/Inventory.gd
+++ detective-game/Inventory.gd
@@ -4,7 +4,11 @@ signal inventory_updated
 
 var items = []
 
-func add_item(item_name):
-	items.append(item_name)
+func add_item(item_name, item_icon):
+	var new_item = {
+		"name": item_name,
+		"icon": item_icon
+	}
+	items.append(new_item)
 	inventory_updated.emit()
-	print("Inventory: ", items)
+	print("Item collected: ", item_name)
--- detective-game/npc_base.gd
+++ detective-game/npc_base.gd
@@ -1,36 +1,14 @@
-extends Area2D
+extends Node2D
 
 @export var npc_name: String = "Person"
-@export var dialogue: DialogueData
-@export var dialogue_interacted: DialogueData
 @export var character_sprite: SpriteFrames
 @export var flip_h: bool = false
 
-@onready var tooltip = $Label
 @onready var sprite = $AnimatedSprite2D
 
-var player_in_range = false
-var interacted = false
 
 func _ready():
-	tooltip.hide()
 	if character_sprite:
 		sprite.sprite_frames = character_sprite
 		sprite.flip_h = flip_h
 		sprite.play("idle")
-
-func _on_body_entered(body):
-	if body.name == "Player":
-		player_in_range = true
-		tooltip.text = "[E] talk to " + npc_name
-		tooltip.show()
-
-func _on_body_exited(body):
-	if body.name == "Player":
-		player_in_range = false
-		tooltip.hide()
-
-func _process(_delta):
-	if player_in_range and Input.is_action_just_pressed("interact"):
-		SignalBus.dialogue_started.emit(dialogue_interacted if interacted else dialogue)
-		interacted = true
