998e4dcb3c3efe00898b2a9592cbee8ea8ca4842
Patrick Winkelholz <patrick.winkelholz@gmail.com>
2026-05-16 16:40:02 +0200

    adding crime apartment + adding choices

--- detective-game/dialogue/dialogue_box/dialogue_box.gd
+++ detective-game/dialogue/dialogue_box/dialogue_box.gd
@@ -1,13 +1,20 @@
 extends CanvasLayer
 
 var skipped = false
+var a_pressed = false
+var b_pressed = false
 
 @onready var portrait := $Box/Portrait
 @onready var text := $Box/Text
+@onready var option_a_text := $Box/OptionA
+@onready var option_b_text := $Box/OptionB
 
 func _ready() -> void:
 	SignalBus.dialogue_started.connect(_on_dialogue_started)
+	option_a_text.visible = false
+	option_b_text.visible = false
 	visible = false
+
 
 func _on_dialogue_started(dialogue: DialogueData):
 	if visible:
@@ -16,15 +23,42 @@ func _on_dialogue_started(dialogue: DialogueData):
 	
 	get_tree().paused = true
 	visible = true
-	for entry in dialogue.dialogue_entries:
+	var entries = dialogue.dialogue_entries
+	for i in entries.size():
+		var entry = entries[i]
 		portrait.texture = entry.character_sprite
-		for line in entry.text:
+		for a in entry.text.size():
+			var line  = entry.text[a]
+			
 			text.visible_characters = 0
 			text.text = line
 			await displayLine(line)
 			
-			#if dialogue.choices:
-				#$Box/OptionA.
+			if dialogue.choices and i == entries.size() - 1 and a == entry.text.size() - 1:
+				if not dialogue.choices.size() == 2:
+					push_error("dialogue with choice must have exactly 2 options!")
+					return
+				
+				print("choice")
+				var option_a = dialogue.choices[0]
+				var option_b = dialogue.choices[1]
+				option_a_text.visible = true
+				option_b_text.visible = true
+				a_pressed = false
+				b_pressed = false
+				option_a_text.text = option_a.text
+				option_b_text.text = option_b.text
+				
+				while not a_pressed and not b_pressed:
+					await get_tree().process_frame
+				
+				option_a_text.visible = false
+				option_b_text.visible = false
+				visible = false
+				get_tree().paused = false
+				
+				SignalBus.dialogue_started.emit(option_a.next_dialogue if a_pressed else option_b.next_dialogue)
+				return
 			
 			while not skipped:
 				await get_tree().process_frame
@@ -32,6 +66,7 @@ func _on_dialogue_started(dialogue: DialogueData):
 			
 	visible = false	
 	get_tree().paused = false
+
 
 func displayLine(line: String):
 	for i in line.length():
@@ -43,6 +78,15 @@ func displayLine(line: String):
 			skipped = false
 			return
 
+
 func _input(p_input_event: InputEvent) -> void:
 	if visible and p_input_event.is_action_pressed(&"skip_dialogue"):
 		skipped = true
+
+
+func _on_option_a_pressed() -> void:
+	a_pressed = true
+
+
+func _on_option_b_pressed() -> void:
+	b_pressed = true
