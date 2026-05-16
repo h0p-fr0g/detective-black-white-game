784bdcdc773f4ac57a675ec5f1bf657d4dfd538c
Patrick Winkelholz <patrick.winkelholz@gmail.com>
2026-05-16 17:05:41 +0200

    adding placeholder portraits + adjusting dialogue box

--- detective-game/dialogue/dialogue_box/dialogue_entry.gd
+++ detective-game/dialogue/dialogue_box/dialogue_entry.gd
@@ -1,5 +1,5 @@
 extends Resource
 class_name DialogueEntry
 
-@export var character_sprite: Texture2D
+@export var character_sprite: Texture2D = preload("res://dialogue/portraits/detective.png")
 @export var text: Array[String]
