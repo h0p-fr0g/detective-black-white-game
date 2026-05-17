3f42d6e80c86b3b1f47d26f5792f9a73d30463d3
h0p-fr0g <martinbraun1997@gmail.com>
2026-05-17 20:18:11 +0200

    finihed second phone dialogue

--- detective-game/npcs/neighbor/neighbor.gd
+++ detective-game/npcs/neighbor/neighbor.gd
@@ -4,11 +4,14 @@ extends NPCBase
 
 
 func _ready():
-	super._ready()
 	if not GlobalFlags.desk_investigated or not GlobalFlags.picture_found:
 		queue_free()
 		return
+	
+	if SignalBus:
+		SignalBus.dialogue_finished.connect(_on_dialogue_finished)
 		
+	super._ready()
 
 
 func _on_interacted() -> void:
@@ -21,8 +24,9 @@ func _on_interacted() -> void:
 	super._on_interacted()
 
 
-
 func _on_dialogue_finished(p_ended_dialogue: Dialogue) -> void:
 	if p_ended_dialogue == late_dialogue:
-		
 		GlobalFlags.second_neighbor_dialogue_finished = true
+		
+		if SignalBus and SignalBus.dialogue_finished.is_connected(_on_dialogue_finished):
+			SignalBus.dialogue_finished.disconnect(_on_dialogue_finished)
