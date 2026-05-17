3d2c0286e56d89ed048ad181a49305cd8c69f1bf
h0p-fr0g <martinbraun1997@gmail.com>
2026-05-17 21:40:51 +0200

    added starting screen

---
+++ detective-game/title_screen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/locations/office.tscn")
+
+func _on_credits_button_pressed() -> void:
+	print("Credits wurden gedrückt!")
