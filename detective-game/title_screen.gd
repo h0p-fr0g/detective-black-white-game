6dd023cc477a2baa20a02cbbc3f412cef4fd582b
h0p-fr0g <martinbraun1997@gmail.com>
2026-05-17 21:59:51 +0200

    title screen

--- detective-game/title_screen.gd
+++ detective-game/title_screen.gd
@@ -4,4 +4,4 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/locations/office.tscn")
 
 func _on_credits_button_pressed() -> void:
-	print("Credits wurden gedrückt!")
+	$Label.visible = true;
