82956e47f0754d56151131330b1857b06064a885
h0p-fr0g <martinbraun1997@gmail.com>
2026-05-17 18:07:56 +0200

    finished second neighbor dialogue

--- detective-game/GlobalFlags.gd
+++ detective-game/GlobalFlags.gd
@@ -6,7 +6,7 @@ var crime_apartment_entered: bool = false
 var first_time_in_apartments: bool = true
 var desk_investigated: bool = false
 var picture_found: bool = false
-var talked_to_clerk: bool = false
+var talked_to_clerk: bool = false   
 
 #Locations
 var location_office_unlocked: bool = true 
--- detective-game/npcs/neighbor/neighbor.gd
+++ detective-game/npcs/neighbor/neighbor.gd
@@ -1,5 +1,7 @@
 extends NPCBase
 
+@export var late_dialogue: Dialogue
+
 
 func _ready():
 	if not GlobalFlags.desk_investigated or not GlobalFlags.picture_found:
@@ -10,4 +12,12 @@ func _ready():
 
 func _on_interacted() -> void:
 	GlobalFlags.location_store_unlocked = true
+	
+	if GlobalFlags.talked_to_clerk:
+		if late_dialogue:
+			print("Clerk-Flag ist aktiv! Wechsle zu Folgedialog.")
+			$Interactable.dialogue = late_dialogue
+		else:
+			print("Warnung: Kein 'clerk_followup_dialogue' im Inspektor zugewiesen!")
+	
 	super._on_interacted()
